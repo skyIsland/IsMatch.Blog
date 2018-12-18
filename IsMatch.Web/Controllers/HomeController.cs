1165102f41a7beb3d84e049831a65ab064e483eb
ismatch <ismatch@qq.com>
2018-12-19 00:16:41 +0800

    首页功能开发

--- IsMatch.Web/Controllers/HomeController.cs
+++ IsMatch.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using IsMatch.Core;
 using Microsoft.AspNetCore.Mvc;
 using IsMatch.Web.Models;
+using XCode;
 
 namespace IsMatch.Web.Controllers
 {
@@ -49,6 +50,75 @@ namespace IsMatch.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult InitData()
+        {
+            ArticleCategory.Meta.BeginTrans();
+            string message = "";
+            try
+            {
+                var articleCategoryList = GetSeedDataBySecondCategory();
+                var articleList = GetSeedDataByArticle();
+                articleCategoryList.Save();
+                articleList.Save();
+                ArticleCategory.Meta.Commit();
+                message = "插入数据出现完成";
+            }
+            catch (Exception ex)
+            {
+                ArticleCategory.Meta.Rollback();
+                message = "插入数据出现错误，原因：" + ex.Message;
+            }
+            return Content(message);
+        }
+
+        public List<ArticleCategory> GetSeedDataBySecondCategory()
+        {
+            ArticleCategory ac = ArticleCategory.Find(ArticleCategory._.KindName == "文章");
+            if (ac == null)
+            {
+                ac = new ArticleCategory
+                {
+                    KindName = "文章",
+                    KindEnName = "index"
+                };
+                ac.Save();
+            }
+            return new List<ArticleCategory>
+            {
+                new ArticleCategory()
+                {
+                    KindName = "前端文章",
+                    KindEnName = "frontend",
+                    PId = ac.Id
+                },
+                new ArticleCategory()
+                {
+                    KindName = "后端文章",
+                    KindEnName = "backend",
+                    PId = ac.Id
+                },
+                new ArticleCategory()
+                {
+                    KindName = "旅游杂记",
+                    KindEnName = "travel",
+                    PId = ac.Id
+                }
+            };
+        }
+
+        public List<Article> GetSeedDataByArticle()
+        {
+            
+            return new List<Article>
+            {
+                new Article()
+                {
+                    Title = "前端系列文章1",
+                    Keyword = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi exercitationem error, quam inventore nobis corporis nam, tenetur quidem voluptatibus similique odit quas molestias? Magni alias enim sint eveniet harum quasi!",
+                }
+            };
         }
     }
 }
---
+++ IsMatch.Web/wwwroot/js/index/list.js
@@ -0,0 +1,9 @@
+﻿layui.define(['laytpl', 'jquery'], function (exports) {
+    var laytpl = layui.laytpl, $ = layui.$;
+    var list = {
+        render: function () {
+           // todo
+        }
+    };
+    exports('list', list);
+});
